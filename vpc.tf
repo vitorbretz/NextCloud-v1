d9acc6f82964a9de83dd61d6b6a86a3cfcfa1ee1
vitorbretz <vitorbretz28@gmail.com>
2025-10-18 13:49:31 -0300

    feat: created subnet in sa-east-1a

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "sp-vpc" {
     Name = "next-cloud-vpc"
   }
 }
+
+resource "aws_subnet" "sub-pub" {
+  vpc_id                  = aws_vpc.sp-vpc.id
+  availability_zone       = "sa-east-1a"
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "next-cloud-subnet-public"
+  }
+}
