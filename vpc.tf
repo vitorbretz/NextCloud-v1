126eaf47692d2013a345856bd02fc48ac7ee5614
vitorbretz <vitorbretz28@gmail.com>
2025-10-18 14:05:15 -0300

    feat: add variables.tf

--- vpc.tf
+++ vpc.tf
@@ -3,16 +3,16 @@ resource "aws_vpc" "sp-vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "next-cloud-vpc"
+    Name = "${var.project_name}-vpc"
   }
 }
 
 resource "aws_subnet" "sub-pub" {
   vpc_id                  = aws_vpc.sp-vpc.id
   availability_zone       = "sa-east-1a"
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
   tags = {
-    Name = "next-cloud-subnet-public"
+    Name = "${var.project_name}-public-subnet"
   }
 }
